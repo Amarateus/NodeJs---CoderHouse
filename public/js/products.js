a7cf98c98cca55128b15cc8011a69c0174142fbb
Amarateus <98360631+Amarateus@users.noreply.github.com>
2023-09-29 14:28:09 -0300

    segunda pre entrega finalizada

--- public/js/products.js
+++ public/js/products.js
@@ -1,19 +1,46 @@
-const button = document.getElementById("addCart")
-console.log(botones)
-const prodId = document.getElementById("id").textContent
+// const button = document.getElementById("addCart")
+// console.log(botones)
+// const prodId = document.getElementById("id").textContent
 
-button.addEventListener("click", async () => {
-    fetch(`http://localhost:8080/api/carts/6516fa112bfd37ed9a8d4267/product/${prodId}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Producto agregado al carrito',
-        showConfirmButton: false,
-        timer: 1500
-    })
-})
+const botones = document.getElementsByClassName("boton")
+const ids = document.getElementsByClassName("id")
+console.log(botones[0].innerHTML)
+console.log(ids[0].innerHTML)
+
+const crear_evento = (boton, id) => {
+    boton.onclick = async () => {
+        fetch(`http://localhost:8080/api/carts/6516fa112bfd37ed9a8d4267/product/${id}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Producto agregado al carrito',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
+}
+
+for (let i = 0; i < botones.length; i++) {
+    crear_evento(botones[i], ids[i].innerHTML);
+}
+
+// button.addEventListener("click", async () => {
+//     fetch(`http://localhost:8080/api/carts/6516fa112bfd37ed9a8d4267/product/${prodId}`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     })
+//     Swal.fire({
+//         position: 'center',
+//         icon: 'success',
+//         title: 'Producto agregado al carrito',
+//         showConfirmButton: false,
+//         timer: 1500
+//     })
+// })
